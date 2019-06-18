9947f3de297aa8f1c04c185fd5ff76c6fa8ebcf3
onedayl <onedayl@Daisy>
2019-06-18 23:05:16 +0800

    增加官方视频#2#3链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    title: 'WS-TRICKS',
+    title: 'WS-Tricks',
     description: 'Web Scraper 从入门到 666',
+    head: [
+      ['link', { rel: 'icon', href: '/favicons.png'}]
+    ],
     themeConfig: {
       nav: [
         { text: '快速上手', link: '/guide/' },
         { text: '完整教程', link: '/tutorial/' },
         { text: '抓取配置', link: '/sitemap/' },
         { text: '辅助工具', link: '/tool/' },
-        { text: 'GitHub', link: 'https://github.com/oneday/wst' },
+        { text: 'GitHub', link: 'https://github.com/onedayl/wst' },
       ],
       sidebar: {
         '/guide/': [
--- docs/README.md
+++ docs/README.md
@@ -9,6 +9,6 @@ features:
 - title: 无限制
   details: 工具下载、功能、频率、导出均无限制，全程免费。
 - title: 强兼容
-  details: 支持抓取绝大多数网站数据，借助浏览器访问不容易被屏蔽。
+  details: 借助浏览器实现跨平台，支持绝大多数网站，不容易被屏蔽。
 footer: MIT Licensed | Copyright © 2019-present onedayl@optional.tech
 ---
